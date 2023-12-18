7dfd7ac13ed0d3e42a43676bd5115efc7331c10c
Judicael <judi.randriamanjato@gmail.com>
2023-12-18 11:04:29 +0300

    chore: adding mocha test

---
+++ TYPESCRIPT/test/main.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Sample Test Suite', () => {
+    it('should return true', () => {
+        const result = true;
+        expect(result).to.be.true;
+    });
+});
